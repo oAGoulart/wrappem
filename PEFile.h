8dd5e5092f0084fad73e6891d94edda43277a876
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2025-05-27 22:53:58 -0300

    Improve indentation

--- PEFile.h
+++ PEFile.h
@@ -10,7 +10,7 @@
 
 class PESectionParameters {
 public:
-  static const int SECTION_DESCRIPTOR_SIZE = 40;
+  static constexpr int SECTION_DESCRIPTOR_SIZE = 40;
 
   std::string name;
   uint32_t virtualAddress;
@@ -37,7 +37,7 @@ public:
     std::ifstream file;
     file.open(filename, std::ios::binary);
 
-    uintmax_t size = FileSize(filename);
+    uintmax_t size = std::filesystem::file_size(filename);
     data_ = new char[size];
     dataSize_ = size;
     file.read(data_, size);
@@ -49,59 +49,75 @@ public:
       if (data_[headerOffset_] == 0x50 && data_[headerOffset_ + 1] == 0x45)
       {
         constexpr int MACHINE_OFFSET = 4;
-        uint16_t machine = *reinterpret_cast<uint16_t*>(&data_[headerOffset_ + MACHINE_OFFSET]);
+        uint16_t machine = *reinterpret_cast<uint16_t*>(
+          &data_[headerOffset_ + MACHINE_OFFSET]);
         if (machine == 0x014c)
         {
-          std::cout << "\t" << __c(42, "[+]") <<
-            "\tMachine: pe32" << std::endl;
+          std::cout << '\t' << __c(42, "[+]") <<
+                       "\tMachine: pe32" << std::endl;
           is64_ = false;
         }
         else if (machine == 0x8664)
         {
           std::cout << '\t' << __c(42, "[+]") <<
-            "\tMachine: pe64" << std::endl;
+                       "\tMachine: pe64" << std::endl;
           is64_ = true;
         }
         else
+        {
          throw std::runtime_error("Unknown machine value");
+        }
 
         constexpr int SECTION_NUM_OFFSET = MACHINE_OFFSET + 2;
-        uint16_t numSections = *reinterpret_cast<uint16_t*>(&data_[headerOffset_ + SECTION_NUM_OFFSET]);
+        uint16_t numSections = *reinterpret_cast<uint16_t*>(
+          &data_[headerOffset_ + SECTION_NUM_OFFSET]);
         std::cout << '\t' << __c(42, "[+]") <<
-          "\tNumber of sections: " << numSections << std::endl;
+                     "\tNumber of sections: " <<
+                     numSections << std::endl;
 
         constexpr int OPT_HEADER_SIZE_OFFSET = SECTION_NUM_OFFSET + 14;
-        uint16_t optHeaderSize = *reinterpret_cast<uint16_t*>(&data_[headerOffset_ + OPT_HEADER_SIZE_OFFSET]);
+        uint16_t optHeaderSize = *reinterpret_cast<uint16_t*>(
+          &data_[headerOffset_ + OPT_HEADER_SIZE_OFFSET]);
         std::cout << '\t' << __c(42, "[+]") <<
-          "\tSize of optional header: " << optHeaderSize << std::endl;
+                     "\tSize of optional header: " <<
+                     optHeaderSize << std::endl;
 
         constexpr int OPT_HEADER_OFFSET = OPT_HEADER_SIZE_OFFSET + 4;
-        uint16_t optHeaderMagic= *reinterpret_cast<uint16_t*>(&data_[headerOffset_ + OPT_HEADER_OFFSET]);
+        uint16_t optHeaderMagic= *reinterpret_cast<uint16_t*>(
+          &data_[headerOffset_ + OPT_HEADER_OFFSET]);
         bool isMagic64;
         if (optHeaderMagic == 0x010b)
         {
           std::cout << '\t' << __c(42, "[+]") <<
-            "\tOptional header magic: 32bit" << std::endl;
+                       "\tOptional header magic: 32bit" << std::endl;
           isMagic64 = false;
         }
         else if (optHeaderMagic == 0x020b)
         {
           std::cout << '\t' << __c(42, "[+]") <<
-            "\tOptional header magic: 64bit" << std::endl;
+                       "\tOptional header magic: 64bit" << std::endl;
           isMagic64 = true;
         }
         else
+        {
           throw std::runtime_error("Unsupported optional header magic value");
+        }
         if (is64_ != isMagic64)
-          throw std::runtime_error("Optional header magic does not match machine");
+        {
+          throw std::runtime_error("Optional header magic"
+                                   "does not match machine");
+        }
 
         constexpr int DATADIR_COUNT = 16;
         constexpr int DATADIR_SIZE = 8;
-        int dataDirsOffset = OPT_HEADER_OFFSET + optHeaderSize - DATADIR_COUNT * DATADIR_SIZE;
+        int dataDirsOffset = OPT_HEADER_OFFSET + optHeaderSize -
+                             DATADIR_COUNT * DATADIR_SIZE;
         importDataDirRVAOffset_ = dataDirsOffset + 8;
-        uint32_t importDataDirRVA = *reinterpret_cast<uint32_t*>(&data_[headerOffset_ + importDataDirRVAOffset_]);
+        uint32_t importDataDirRVA = *reinterpret_cast<uint32_t*>(
+          &data_[headerOffset_ + importDataDirRVAOffset_]);
         importDataDirSizeOffset_ = importDataDirRVAOffset_ + 4;
-        uint32_t importDataDirSize = *reinterpret_cast<uint32_t*>(&data_[headerOffset_ + importDataDirSizeOffset_]);
+        uint32_t importDataDirSize = *reinterpret_cast<uint32_t*>(
+          &data_[headerOffset_ + importDataDirSizeOffset_]);
 
         sectionsOffset_ = OPT_HEADER_OFFSET + optHeaderSize;
         int32_t index = sectionsOffset_;
@@ -111,7 +127,8 @@ public:
           index += PESectionParameters::SECTION_DESCRIPTOR_SIZE;
           sectionsParams_.push_back(section);
           std::cout << '\t' << __c(42, "[+]") <<
-            "\tFound section: " << section.name << std::endl;
+                       "\tFound section: " <<
+                       section.name << std::endl;
         }
 
         bool foundImportSection = false;
@@ -122,20 +139,28 @@ public:
               section->virtualAddress + section->rawDataSize >=
               importDataDirRVA + importDataDirSize)
           {
-            uint32_t importDataDirOffset = importDataDirRVA - section->virtualAddress + section->rawDataAddress;
+            uint32_t importDataDirOffset = importDataDirRVA -
+                                           section->virtualAddress +
+                                           section->rawDataAddress;
             importData_ = new char[importDataDirSize];
             importDataSize_ = importDataDirSize;
-            if (memcpy_s(importData_, importDataDirSize, data_ + importDataDirOffset, importDataDirSize) != 0)
+            if (memcpy_s(importData_, importDataDirSize, data_ +
+              importDataDirOffset, importDataDirSize) != 0)
+            {
               throw std::runtime_error("Memory copying issue");
+            }
 
             std::cout << '\t' << __c(42, "[+]") <<
-              "\tImport found at section: " << section->name << std::endl;
+                         "\tImport found at section: " <<
+                         section->name << std::endl;
             foundImportSection = true;
             break;
           }
         }
         if (!foundImportSection)
+        {
           throw std::runtime_error("Unable to find import section");
+        }
         return;
       }
     }
@@ -145,9 +170,13 @@ public:
   ~PEFile()
   {
     if (data_ != nullptr)
+    {
       delete data_;
+    }
     if (importData_ != nullptr)
+    {
       delete importData_;
+    }
   }
 
   uint32_t
@@ -176,7 +205,8 @@ public:
     uint32_t n = 0;
     memcpy_s(&buff[n], size - n, importData_, importDataSize_ - 20);
     n += static_cast<uint32_t>(importDataSize_ - 20);
-    uint32_t v = static_cast<uint32_t>(baseAddress + importDataSize_ + 40 +
+    uint32_t v = static_cast<uint32_t>(
+      baseAddress + importDataSize_ + 40 +
       payloadDll.length() + dummyFunc.length());
     memcpy_s(&buff[n], size - n, &v, 4);
     n += 4;
@@ -185,7 +215,8 @@ public:
     v = static_cast<uint32_t>(baseAddress + importDataSize_ + 20);
     memcpy_s(&buff[n], size - n, &v, 4);
     n += 4;
-    v = static_cast<uint32_t>(baseAddress + importDataSize_ + 24 +
+    v = static_cast<uint32_t>(
+      baseAddress + importDataSize_ + 24 +
       payloadDll.length() + dummyFunc.length());
     memcpy_s(&buff[n], size - n, &v, 4);
     n += 4;
@@ -197,21 +228,22 @@ public:
     n += 2;
     memcpy_s(&buff[n], size - n, dummyFunc.c_str(), dummyFunc.length() + 1);
     n += static_cast<uint32_t>(dummyFunc.length() + 1);
-    v = static_cast<uint32_t>(baseAddress + importDataSize_ + 21 +
-      payloadDll.length());
+    v = static_cast<uint32_t>(
+      baseAddress + importDataSize_ + 21 + payloadDll.length());
     memcpy_s(&buff[n], size - n, &v, 4);
     n += 4;
     memset(&buff[n], '\0', 12);
     n += 12;
-    v = static_cast<uint32_t>(baseAddress + importDataSize_ + 21 +
-      payloadDll.length());
+    v = static_cast<uint32_t>(
+      baseAddress + importDataSize_ + 21 + payloadDll.length());
     memcpy_s(&buff[n], size - n, &v, 4);
     n += 4;
     memset(&buff[n], '\0', 12);
     n += 12;
 
     uint32_t beginSize = sectionsParams_.back().rawDataAddress + originalSize;
-    uint32_t beginAlign = sectionsParams_.back().rawDataAddress + Align_(originalSize, 0x1000);
+    uint32_t beginAlign = sectionsParams_.back().rawDataAddress +
+                          Align_(originalSize, 0x1000);
     uint32_t endAlign = Align_(n, 0x200);
 
     uint32_t newDataSize = beginAlign + endAlign;
@@ -223,12 +255,14 @@ public:
     data_ = newData;
     dataSize_ = newDataSize;
     std::cout << '\t' << __c(42, "[+]") <<
-      "\tNew file size: " << dataSize_ << std::endl;
+                 "\tNew file size: " << dataSize_ << std::endl;
 
     memcpy_s(data_, dataSize_, &baseAddress, 4);
-    uint32_t newImportSize = *reinterpret_cast<uint32_t*>(&data_[headerOffset_ + importDataDirSizeOffset_]);
+    uint32_t newImportSize = *reinterpret_cast<uint32_t*>(
+      &data_[headerOffset_ + importDataDirSizeOffset_]);
     newImportSize += 0x14;
-    memcpy_s(&data_[headerOffset_ + importDataDirSizeOffset_], dataSize_, &newImportSize, 4);
+    memcpy_s(&data_[headerOffset_ + importDataDirSizeOffset_],
+             dataSize_, &newImportSize, 4);
     FixSizeOfImage_();
   }
 
@@ -236,7 +270,10 @@ public:
   {
     std::string outPath = filename;
     auto dirPath = outPath.substr(0, outPath.find_last_of("\\/"));
-    if (!dirPath.empty()) MkDir(&dirPath.front());
+    if (!dirPath.empty())
+    {
+      std::filesystem::create_directory(&dirPath.front());
+    }
 
     std::ofstream ofile;
     ofile.open(filename, std::ios::binary);
@@ -258,24 +295,30 @@ private:
 
   void FixSizeOfImage_()
   {
-    uint32_t size = sectionsParams_.back().virtualSize + sectionsParams_.back().virtualAddress;
+    uint32_t size = sectionsParams_.back().virtualSize +
+                    sectionsParams_.back().virtualAddress;
     if (size % 0x1000 != 0)
+    {
       size = ((size / 0x1000) + 1) * 0x1000;
+    }
     *reinterpret_cast<uint32_t*>(&data_[headerOffset_ + 80]) = size;
     std::cout << '\t' << __c(42, "[+]") <<
-      "\tFixed size of image" << std::endl;
+                 "\tFixed size of image" << std::endl;
   }
 
   uint32_t CalculateNewImportSize_(std::string payloadDll, std::string dummyFunc)
   {
-    return static_cast<uint32_t>(importDataSize_ + 56 +
-      payloadDll.length() + dummyFunc.length());
+    return static_cast<uint32_t>(
+      importDataSize_ + 56 + payloadDll.length() + dummyFunc.length());
   }
 
   uint32_t Align_(uint32_t length, uint32_t value)
   {
     uint32_t n = 0;
-    while (n < length) n += value;
+    while (n < length)
+    {
+      n += value;
+    }
     return n;
   }
 };
