d70a24dd820f44ea02312432615727e522d3c372
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2025-05-27 22:56:07 -0300

    Remove inline functions

--- shared.h
+++ shared.h
@@ -7,13 +7,3 @@
 #define __str(s) #s
 #define __xstr(s) __str(s)
 #define __c(c, str) "\033[" __str(c) "m" str "\033[m"
-
-inline uintmax_t FileSize(const char* filename)
-{
-  return std::filesystem::file_size(filename);
-}
-
-inline void MkDir(const char* filename)
-{
-  std::filesystem::create_directory(filename);
-}
