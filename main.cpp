f6cb874d62c0eebdf823c8daf8ee993334b2e73b
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2025-06-09 21:24:03 -0300

    Update files

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include "PEFormat.h"
-//#include "PEFile.h"
 #include <memory>
 
 int main()
 {
   auto f = std::make_unique<wrappem::PatchPE>(
            "./version.dll", "yasl.dll", "Dummy");
-  //auto f = std::make_unique<PEFile>("./dinput8.dll");
-  //f->CreateNewImport("yasl.dll", "Dummy");
-  //f->Save("./dinput8-payload.dll");
 
   return 0;
 }
